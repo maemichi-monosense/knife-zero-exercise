0663ec3591ca6ea9d7e31e660d7984f4f5496826
yuya-m-ms <maemichi@monosense.co.jp>
2016-07-05 17:03:01 +0900

    Add whitelist to reduce node object size

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,1 +1,22 @@
 local_mode true
+
+knife[:automatic_attribute_whitelist] = [
+  "fqdn/",
+  "os/",
+  "os_version/",
+  "hostname",
+  "ipaddress/",
+  "roles/",
+  "recipes/",
+  "ipaddress/",
+  "platform/",
+  "platform_version/",
+  "platform_version/",
+  "cloud/",
+  "cloud_v2/",
+  "ec2/ami_id/",
+  "ec2/instance_id/",
+  "ec2/instance_type/",
+  "ec2/placement_availability_zone/",
+  "chef_packages/"
+]
